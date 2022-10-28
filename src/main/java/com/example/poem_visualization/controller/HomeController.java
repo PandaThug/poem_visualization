39243cc57b56e75e30d781023863251ac18f94ed
CDConlyred <1746904635@qq.com>
2022-10-28 20:49:24 +0800

    首页路由

---
+++ src/main/java/com/example/poem_visualization/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.poem_visualization.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage() {
+        return "/index";
+    }
+
+}
