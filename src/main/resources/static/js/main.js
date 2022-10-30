d0d1c53e66c4d8a428fcda2c5cf063a9e3923686
Tantanz0918 <297752020@qq.com>
2022-10-30 16:35:51 +0800

    地图嵌入和时间轴可用

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,27 +1,122 @@
-//使用对象是形如这样的字符串：{name=哈哈, value=3}
-function stringtoObj1(str){
-    var str2=str.substring(1,str.length-1);
-    let obj = {};
-    let arr1 = str2.split(', ');
-    arr1.forEach(function(item){
-        //每一项元素，再根据=号字符 打散成数组
-        let kv = item.split('=')
-        //每次向对象里面添加 属性和属性值
-        obj[kv[0]] = kv[1]
-    })
-    return obj;
+//全局变量的定义和初始化视图
+//控制全局的模式
+let timeIndex=0;//0为”全局“
+let poetName="";//空字符串为”全选“
+// mode0：全部全局模式。
+// mode1：未指定诗人，指定时间。
+// mode2：未指定时间，指定诗人。
+// mode3：指定时间，指定诗人。
+let showMode=0;
+
+var timelineYear = ["全局"];//来了老弟
+for(i=610;i<907;i+=10)
+{
+    timelineYear.push(i+"s");
+}
+console.log(timelineYear)
+
+//全局可能会用到的函数
+function changeTimeIndex(index){
+    timeIndex=index;
+    changeMode();
+}
+function changePoetName(name){
+    poetName=name;
+    changeMode();
+}
+function changeMode(){
+    //计算模式
+    if(timeIndex==0)
+    {
+        if(poetName=="")
+            showMode=0;
+        else
+            showMode=2;
+    }
+    else
+    {
+        if(poetName=="")
+            showMode=1;
+        else
+            showMode=3;
+    }
+    console.log("模式转变为"+showMode)
+}
+//将单人路径转化为一个全是字符串的数组
+function footobj(obj){
+    let foots=obj['places'].split(",");
+    console.log("单词处理路径");
+    console.log(foots);
+    return foots;
+}
+//将多人路径转换为字符串数组的数组
+function footobj_plus(obj){
+    let result=[];
+    for(i=0;i<obj.length;i++)
+    {
+        let foots=footobj(obj[i]);
+        result.push(foots);
+    }
+    console.log("多人路径结果");
+    console.log(result);
+    return result;
 }
 
-////使用对象是形如这样的字符串：{雪=1, 风=2}，注意返回的应该是数组：[{name=雪, value=1},{name=风, value=2}]
-function stringtoObj2(str){
-    var str2=str.substring(1,str.length-1);
-    let obj = {};
-    let arr1 = str2.split(', ');
-    arr1.forEach(function(item){
-        //每一项元素，再根据=号字符 打散成数组
-        let kv = item.split('=')
-        //每次向对象里面添加 属性和属性值
-        obj[kv[0]] = kv[1]
-    })
-    return obj;
+var convertData = function(data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        for(var j=0;j<data[i].length;j++)
+        {
+            var geoCoord = geoCoordMap[data[i][j]];
+            if (geoCoord) {
+                res.push({
+                    name: data[i][j],
+                    //字符串拼接
+                    //value最终值：[x轴,y轴,value]
+                    value: geoCoord.concat(100)
+                });
+            }
+        }
+
+    }
+
+    return res;
+};
+
+var convertToLineData = function(data, gps) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        for(var j=0;j<data[i].length;j++){
+            var dataItem = data[i][j];
+            //出发点
+            var fromCoord = geoCoordMap[dataItem];
+            if(data[i][j].length==1)
+                var toCoord = geoCoordMap[dataItem];//就是原来的
+            else
+                var toCoord = geoCoordMap[data[i][j+1]]; //下一个
+
+            //  var toCoord = geoGps[Math.random()*3];
+            if (fromCoord && toCoord) {
+                res.push([{
+                    //为什么还要出发地的value，测试了一下好像值不影响图像的任意指标
+                    coord: fromCoord,
+                }, {
+                    coord: toCoord,
+                }]);
+            }
+        }
+
+    }
+    console.log(res);
+    //
+    return res;
+};
+
+//转换timeIndex为具体的时间
+var convertTimeIndex=function(){
+    if(timeIndex==1)
+        return 618;
+    else{
+        return (timeIndex+60)*10;
+    }
 }
---
+++ src/main/resources/static/js/timeline.js
@@ -0,0 +1,78 @@
+
+timelineOption={
+    timeline: {
+        data: timelineYear,
+        inverse:true,
+        currentIndex:0,
+        orient:'vertical',
+        axisType: 'category',
+        autoPlay: false,
+        playInterval: 3000,
+        left: '10%',
+        right: '10%',
+        bottom: '2%',
+        width: '70%',
+        height:'96%',
+
+        label: {
+            normal: {
+                textStyle: {
+                    color: '#555'
+                }
+            },
+            emphasis: {
+                textStyle: {
+                    color: '#333',
+                    fontWeight: "bolder"
+
+                }
+            }
+        },
+        symbolSize: 10,
+        lineStyle: {
+            color: '#555'
+        },
+        checkpointStyle: {
+            borderColor: '#777',
+            borderWidth: 2
+        },
+        controlStyle: {
+            showPlayBtn: false,
+            showNextBtn: false,
+            showPrevBtn: false,
+            normal: {
+                color: '#666',
+                borderColor: '#666'
+            },
+            emphasis: {
+                color: '#aaa',
+                borderColor: '#aaa'
+            }
+        },
+
+    },
+
+}
+var timeLine = echarts.init(document.querySelector('#timeAxis'));
+timeLine.setOption(timelineOption)
+
+document.getElementById("globalBtn").onclick = function(){
+    //点击后，map隐藏时间轴移动到全局
+    console.log("!");
+    changeTimeIndex(0);
+    timeOption={
+        timeline:{
+            currentIndex:0
+        }
+    }
+    timeLine.setOption(timeOption)
+    document.querySelector(".timeAxis").scrollTop=0;
+    updateMap();
+};
+timeLine.on('timelineChanged', function (params) {
+    // 控制台打印数据的名称
+    yearIndex=params.currentIndex;//此为年份索引
+    changeTimeIndex(yearIndex);
+    updateMap()
+
+});
