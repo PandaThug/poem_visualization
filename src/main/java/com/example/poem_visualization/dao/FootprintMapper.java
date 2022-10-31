48f3eb0d1f48e661d5c718b5491c03d2b6b38992
CDConlyred <1746904635@qq.com>
2022-10-31 23:49:12 +0800

    查询诗人及其路径debug：废弃之前按时间查全部诗人的方法，改为先查出诗人姓名，再根据时间和姓名查询所有诗人指定时间的路径。

--- src/main/java/com/example/poem_visualization/dao/FootprintMapper.java
+++ src/main/java/com/example/poem_visualization/dao/FootprintMapper.java
@@ -13,12 +13,15 @@ public interface FootprintMapper {
     List<Map<String, String>> selectFootprints();
 
     // 查询指定时间全部诗人的路线
-    List<Map<String, String>> selectFootprintsByTime(@Param("begin") int begin, @Param("end") int end);
+//    List<Map<String, String>> selectFootprintsByTime(@Param("begin") int begin, @Param("end") int end);
 
     // 查询指定诗人全部时间的路线
     Map<String, String> selectFootprintsByName(@Param("name") String name);
 
     // 查询指定诗人指定时间的路线
     Map<String, String> selectFootprintByNameAndTime(@Param("name") String name, @Param("begin") int begin, @Param("end") int end);
+
+    // 查询所有的诗人名字
+    List<String> selectPoemNames();
 
 }
--- src/main/java/com/example/poem_visualization/service/FootprintService.java
+++ src/main/java/com/example/poem_visualization/service/FootprintService.java
@@ -4,6 +4,7 @@ import com.example.poem_visualization.dao.FootprintMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,7 +21,18 @@ public class FootprintService {
 
     // 查询指定时间全部诗人的路线
     public List<Map<String, String>> findFootprintsByTime(int beginTime, int endTime) {
-        return footprintMapper.selectFootprintsByTime(beginTime, endTime);
+//        return footprintMapper.selectFootprintsByTime(beginTime, endTime);
+
+        List<Map<String, String>> maps = new ArrayList<>();
+        List<String> names = footprintMapper.selectPoemNames();
+        Map<String, String> map;
+        for (String name : names) {
+            map = footprintMapper.selectFootprintByNameAndTime(name, 780, 820);
+            maps.add(map);
+        }
+
+        return maps;
+
     }
 
     // 查询指定诗人全部时间的路线
