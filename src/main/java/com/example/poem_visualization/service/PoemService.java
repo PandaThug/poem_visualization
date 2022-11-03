739441fb18374b6616842b18a6224c0bcc76a143
CDConlyred <1746904635@qq.com>
2022-11-03 21:34:19 +0800

    空值处理

--- src/main/java/com/example/poem_visualization/service/PoemService.java
+++ src/main/java/com/example/poem_visualization/service/PoemService.java
@@ -4,6 +4,7 @@ import com.example.poem_visualization.dao.PoemMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,62 +16,160 @@ public class PoemService {
 
     // 根据诗人姓名获取诗歌意象——名称和次数
     public List<Map<String, Integer>> findImagesByPoet(String poetName) {
-        return poemMapper.selectImagesByPoet(poetName);
+        List<Map<String, Integer>> maps = poemMapper.selectImagesByPoet(poetName);
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 根据时间获取诗歌意象——名称和次数
     public List<Map<String, Integer>> findImagesByTime(Integer beginTime, Integer endTime) {
-        return poemMapper.selectImagesByTime(beginTime, endTime);
+        List<Map<String, Integer>> maps = poemMapper.selectImagesByTime(beginTime, endTime);
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 获取全部诗歌意象——名称和次数
     public List<Map<String, Integer>> findImages() {
-        return poemMapper.selectImages();
+        List<Map<String, Integer>> maps = poemMapper.selectImages();
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 根据时间和诗人姓名获取诗歌意象——名称和次数
-    public List<Map<String, Integer>> findImagesByPoetAndTime(String poetName, Integer beginTime, Integer endTime) {
-        return poemMapper.selectImagesByPoetAndTime(poetName, beginTime, endTime);
+    public List<Map<String, Integer>> findImagesByPoetAndTime(String poetName, Integer beginTime,
+                                                              Integer endTime) {
+        List<Map<String, Integer>> maps = poemMapper.selectImagesByPoetAndTime(poetName, beginTime, endTime);
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 根据诗人姓名获取诗歌情感——名称和次数
     public List<Map<String, Integer>> findEmoByPoet(String poetName) {
-        return poemMapper.selectEmoByPoet(poetName);
+        List<Map<String, Integer>> maps = poemMapper.selectEmoByPoet(poetName);
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 根据时间获取诗歌情感——名称和次数
     public List<Map<String, Integer>> findEmoByTime(Integer beginTime, Integer endTime) {
-        return poemMapper.selectEmoByTime(beginTime, endTime);
+        List<Map<String, Integer>> maps = poemMapper.selectEmoByTime(beginTime, endTime);
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 获取全部诗歌情感——名称和次数
     public List<Map<String, Integer>> findEmo() {
-        return poemMapper.selectEmo();
+        List<Map<String, Integer>> maps = poemMapper.selectEmo();
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 根据时间和诗人姓名获取诗歌情感——名称和次数
-    public List<Map<String, Integer>> findEmoByPoetAndTime(String poetName, Integer beginTime, Integer endTime) {
-        return poemMapper.selectEmoByPoetAndTime(poetName, beginTime, endTime);
+    public List<Map<String, Integer>> findEmoByPoetAndTime(String poetName, Integer beginTime,
+                                                           Integer endTime) {
+        List<Map<String, Integer>> maps = poemMapper.selectEmoByPoetAndTime(poetName, beginTime, endTime);
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 获取包含指定意象的不同情感的诗歌数量——通过意象词、诗人姓名、时间查询
-    public List<Map<String, Integer>> findPoemImagesCountByNameAndTime(String poetName, Integer beginTime, Integer endTime, String images) {
-        return poemMapper.selectPoemImagesCountByNameAndTime(poetName, beginTime, endTime, images);
+    public List<Map<String, Integer>> findPoemImagesCountByNameAndTime(String poetName, Integer beginTime,
+                                                                       Integer endTime, String images) {
+        List<Map<String, Integer>> maps = poemMapper
+                .selectPoemImagesCountByNameAndTime(poetName, beginTime, endTime, images);
+        // 空值处理
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 获取包含指定意象的不同情感的诗歌数量——通过意象词、时间查询
-    public List<Map<String, Integer>> findPoemImagesCountByTime(Integer beginTime, Integer endTime, String images) {
-        return poemMapper.selectPoemImagesCountByTime(beginTime, endTime, images);
+    public List<Map<String, Integer>> findPoemImagesCountByTime(Integer beginTime, Integer endTime,
+                                                                String images) {
+        List<Map<String, Integer>> maps = poemMapper.selectPoemImagesCountByTime(beginTime, endTime, images);
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 获取包含指定意象的不同情感的诗歌数量——通过意象词、诗人姓名查询
     public List<Map<String, Integer>> findPoemImagesCountByName(String poetName, String images) {
-        return poemMapper.selectPoemImagesCountByName(poetName, images);
+        List<Map<String, Integer>> maps = poemMapper.selectPoemImagesCountByName(poetName, images);
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
     // 获取包含指定意象的不同情感的诗歌数量——通过意象词查询
     public List<Map<String, Integer>> findPoemImagesCount(String images) {
-        return poemMapper.selectPoemImagesCount(images);
+        List<Map<String, Integer>> maps = poemMapper.selectPoemImagesCount(images);
+        if (maps.get(0) == null) {
+            maps.remove(0);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("msg", 0);
+            maps.add(map);
+        }
+        return maps;
     }
 
 }
