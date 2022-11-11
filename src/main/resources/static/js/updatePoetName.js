0dcbd572995e666a0ea710531393499f99e2ac89
Tantanz0918 <297752020@qq.com>
2022-11-11 22:38:38 +0800

    修复一些bug，悬浮窗待添加

--- src/main/resources/static/js/updatePoetName.js
+++ src/main/resources/static/js/updatePoetName.js
@@ -1,15 +1,17 @@
 //由于选择名字时不仅要改变后台数据中poetName的值，还要调用一系列更新函数，因此在这个js文件中单独定义
 function updatePoetName(name){
     poetName=name;
-    updateAll();
-    axios('http://localhost:8080/visualization/poet/lifetime?name='+name)
-        .then(function (response) {
-           let birth=response.data['birth'];
-           let death=response.data['death'];
-            document.getElementById("timeBegin").value=birth;
-            document.getElementById("timeEnd").value=death;
-        }).catch(function (err) {
-        console.log(err)
-    });
+    changeMode();
+    if(name!="") {
+        axios('http://localhost:8080/visualization/poet/lifetime?name=' + name)
+            .then(function (response) {
+                let birth = response.data['birth'];
+                let death = response.data['death'];
+                document.getElementById("timeBegin").value = birth;
+                document.getElementById("timeEnd").value = death;
+            }).catch(function (err) {
+            console.log(err)
+        });
+    }
 
 }
