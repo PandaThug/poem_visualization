877123ef917cb55989c29aaaf111d6a4571f6565
Tantanz0918 <297752020@qq.com>
2022-11-11 22:15:55 +0800

    修复一些bug，悬浮窗待添加

--- src/main/resources/static/js/updateLeaf.js
+++ src/main/resources/static/js/updateLeaf.js
@@ -1,5 +1,6 @@
 //更新树叶图的数据调用函数
-let getNum=0;//axios请求完成数量
+let getNum=0;//axios请求发起数量
+let receiveNum=0;
 let getSum=0;//axios请求需要完成的总数量
 function updateLeaf()
 {
@@ -59,6 +60,7 @@ function updateLeaf2(response){
     dataTest=dataTest.slice(1,16);//截取1-(n-1)名
     getNum=0;
     getSum=dataTest.length;
+    receiveNum=0;
     let option = pieChart.getOption();
     if(showMode==0)
     {//
@@ -123,6 +125,7 @@ function updateLeaf2(response){
 
 function updateLeaf3(response,imageName){
     let dataLeaf=[];
+    receiveNum++;
     let num=0;
     for(key in response.data){
         let obj={};
@@ -144,7 +147,7 @@ function updateLeaf3(response,imageName){
     console.log("第"+(num+2)/2+"个意象");
     console.log(imageName);
     console.log(dataLeaf);
-    if(getNum==num*2-2)//获取完毕，push设置
+    if(receiveNum==getSum)//获取完毕，push设置
     {
         pieChart.setOption(option);
     }
--- src/main/resources/static/js/updatePoetName.js
+++ src/main/resources/static/js/updatePoetName.js
@@ -1,31 +1,13 @@
 //由于选择名字时不仅要改变后台数据中poetName的值，还要调用一系列更新函数，因此在这个js文件中单独定义
 function updatePoetName(name){
     poetName=name;
-    //树叶图气泡图需要在时间被指定前进行更新
-    showMode=2;
-    updateLeaf();
-    updateBubbles();
-    //map和linechart需要指定时间，为了防止直接调用main.js中的timesetting()导致的所有图更新，在此处单独写一份类似的流程
+    updateAll();
     axios('http://localhost:8080/visualization/poet/lifetime?name='+name)
         .then(function (response) {
            let birth=response.data['birth'];
            let death=response.data['death'];
             document.getElementById("timeBegin").value=birth;
             document.getElementById("timeEnd").value=death;
-            beginTime=Number(birth);
-            endTime=Number(death);
-            timeIndex=31;//时间轴跳转到“自定义”
-            timeOption={
-                timeline:{
-                    currentIndex:31
-                }
-            }
-            timeLine.setOption(timeOption)
-            let scl=document.querySelector(".timeAxis")
-            scl.scrollTop=scl.scrollHeight;//滑动至底部
-            showMode=3;
-            updateMap();
-            updateLineChart();
         }).catch(function (err) {
         console.log(err)
     });
